956d7076526aea24b697fcdfdd46220a95402382
unknown <fedoroffs@yahoo.com>
2021-04-14 18:42:19 -0700

    proyecto final

---
+++ main.js
@@ -0,0 +1,13 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
